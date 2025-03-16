1dff68f166b45074f57344f0241c53e0c2c9096c
doodtato <jeremyowen2009@gmail.com>
2025-03-15 19:18:43 -0500

    complete bookbot

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from stats import get_num_words
 from stats import count_characters
 from stats import sort_dict
+import sys
 
 def get_book_text():
-    with open("books/frankenstein.txt") as book:
+    with open(f"{sys.argv[1]}") as book:
         book_contents = book.read()
     return book_contents
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     book = get_book_text()
     word_count = get_num_words(book)
     print("============ BOOKBOT ============")
